e6aef848947111d5007fc7f25e7d3f581426aba0
flavio <dkscript@gmail.com>
2017-12-15 00:05:08 -0200

    aprendendo python

--- teste.py
+++ teste.py
@@ -1,3 +1,7 @@
 #! /usr/bin/env python3
-print("novo teste de desenvolvimento")
+nome = input ('Qual é o seu nome? ')
+idade = input ('Qual é a sua idade? ')
+peso = input ('Qual é o seu peso ? ')
+
+print(nome,idade,peso)
 
