f91a78c018648d7efce926b16a8a0b9668500e69
dkscript <dkscript@gmail.com>
2017-12-28 22:29:44 -0200

    update, files

--- desafio13.py
+++ desafio13.py
@@ -2,3 +2,4 @@
 
 salario = float(input('Digite o seu salario atual: '))
 print('Seu salário foi reajustado com aumento de 15% R$ {}'.format(salario * 1.15))
+print('teste de digiteção com teclado novo , tenho que me acostumar{} [][][{}{')
