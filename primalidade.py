5e0cfa0212c5139a1930efa967bf251fe695e807
flavio nunes <dkscript@gmail.com>
2018-01-16 07:55:12 -0200

    atualizando scripts python

--- primalidade.py
+++ primalidade.py
@@ -10,17 +10,18 @@ primalidade = True
 primo = 'é'
 
 #numero = int(input('Digite um numero interio positivo: '))
-numero = 1000000
+numero = 15
 print('1')
 
 while True:
-    numero = numero + 1
+    #numero = numero + 1
     i = 2
     while i != numero:
         if numero % i == 0:
             primalidade = False
             primo = 'não é'
         i = i + 1
+        print(' i - ',i)
        
     if primalidade:
         print(numero)
