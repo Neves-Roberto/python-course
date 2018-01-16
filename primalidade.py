ae623e7ce56ee46c3135fa423793878c4bf43b82
flavio nunes <dkscript@gmail.com>
2018-01-16 05:19:29 -0200

    atualizando scripts python

---
+++ primalidade.py
@@ -0,0 +1,27 @@
+#2, 3, 5, 7, 11 ...
+
+'''
+algoritimo generico para descobrir numeros primos
+ler um numero inteiro positivo
+
+'''
+
+primalidade = True
+primo = 'é'
+
+#numero = int(input('Digite um numero interio positivo: '))
+numero = 1000000
+print('1')
+
+while True:
+    numero = numero + 1
+    i = 2
+    while i != numero:
+        if numero % i == 0:
+            primalidade = False
+            primo = 'não é'
+        i = i + 1
+       
+    if primalidade:
+        print(numero)
+    primalidade = True
