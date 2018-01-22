87b7ea623701db07da056602853711da13c6dfd4
flavio nunes <dkscript@gmail.com>
2018-01-22 09:09:58 -0200

    atualizando scripts python

---
+++ conta_primos.py
@@ -0,0 +1,11 @@
+'''
+Exercício 1 - Primos
+
+Escreva a função n_primos que recebe um número inteiro maior ou igual a 2
+como parâmetro e devolve a quantidade de números primos que existem entre 2
+e n (incluindo 2 e, se for o caso, n).
+'''
+
+def n_primos(n):
+    pass
+
