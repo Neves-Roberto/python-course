2920be5851ce0b4972ea93aa75ce1070d79b5b17
dkscript <dkscript@gmail.com>
2018-01-24 05:44:19 -0500

    Atualizado do Script

---
+++ sobe_git.sh
@@ -0,0 +1,11 @@
+echo "Subindo arquivos para o GIT"
+echo .
+echo .
+echo .
+echo "Adicionando todos os arquivos"
+git add -A
+echo "Commit dos arquivos no Branch"
+git commit -am "Atualizado do Script"
+echo "Publicando os arquivos no github"
+git push
+echo "Fim do processo.."
