4d4628d27254d9d874f6d7f644ae4c935c19664f
flavio nunes <dkscript@gmail.com>
2018-01-31 07:38:28 -0200

    atualizando scripts python

--- coh_piah.py
+++ coh_piah.py
@@ -1,9 +1,9 @@
 import re
 
 def le_assinatura():
-  '''A funcao le os valores dos tracos linguisticos do modelo e devolve uma assinatura a ser comparada com os textos fornecidos'''
+    '''A funcao le os valores dos tracos linguisticos do modelo e devolve uma assinatura
+    a ser comparada com os textos fornecidos'''
     print("Bem-vindo ao detector automático de COH-PIAH.")
-
     wal = float(input("Entre o tamanho medio de palavra:"))
     ttr = float(input("Entre a relação Type-Token:"))
     hlr = float(input("Entre a Razão Hapax Legomana:"))
@@ -32,7 +32,8 @@ def separa_sentencas(texto):
     return sentencas
 
 def separa_frases(sentenca):
-    '''A funcao recebe uma sentenca e devolve uma lista das frases dentro da sentenca'''
+    '''A funcao recebe uma sentenca e devolve uma lista das frases dentro da
+    sentenca'''
     return re.split(r'[,:;]+', sentenca)
 
 def separa_palavras(frase):
@@ -40,7 +41,8 @@ def separa_palavras(frase):
     return frase.split()
 
 def n_palavras_unicas(lista_palavras):
-    '''Essa funcao recebe uma lista de palavras e devolve o numero de palavras que aparecem uma unica vez'''
+    '''Essa funcao recebe uma lista de palavras e devolve o numero de palavras que
+    aparecem uma unica vez'''
     freq = dict()
     unicas = 0
     for palavra in lista_palavras:
@@ -56,7 +58,8 @@ def n_palavras_unicas(lista_palavras):
     return unicas
 
 def n_palavras_diferentes(lista_palavras):
-    '''Essa funcao recebe uma lista de palavras e devolve o numero de palavras diferentes utilizadas'''
+    '''Essa funcao recebe uma lista de palavras e devolve o numero de palavras
+    diferentes utilizadas'''
     freq = dict()
     for palavra in lista_palavras:
         p = palavra.lower()
@@ -68,13 +71,101 @@ def n_palavras_diferentes(lista_palavras):
     return len(freq)
 
 def compara_assinatura(as_a, as_b):
-    '''IMPLEMENTAR. Essa funcao recebe duas assinaturas de texto e deve devolver o grau de similaridade nas assinaturas.'''
+    '''IMPLEMENTAR. Essa funcao recebe duas assinaturas de texto e deve devolver o grau
+    de similaridade nas assinaturas.'''
     pass
 
 def calcula_assinatura(texto):
-    '''IMPLEMENTAR. Essa funcao recebe um texto e deve devolver a assinatura do texto.'''
+    '''IMPLEMENTAR. Essa funcao recebe um texto e deve devolver a assinatura do
+    texto.'''
     pass
 
 def avalia_textos(textos, ass_cp):
-    '''IMPLEMENTAR. Essa funcao recebe uma lista de textos e deve devolver o numero (1 a n) do texto com maior probabilidade de ter sido infectado por COH-PIAH.'''
+    '''IMPLEMENTAR. Essa funcao recebe uma lista de textos e deve devolver o
+    numero (1 a n) do texto com maior probabilidade de ter sido infectado
+    por COH-PIAH.'''
     pass
+
+    
+
+def main0():
+    #textos = le_textos()# faz a leitura dos textos e armazena em textos
+    #textos = ['(1)Navegadores antigos tinham uma frase gloriosa:"Navegar é preciso; viver não é preciso". Quero para mim o espírito [d]esta frase, transformada a forma para a casar como eu sou: Viver não é necessário; o que é necessário é criar. Não conto gozar a minha vida; nem em gozá-la penso. Só quero torná-la grande,ainda que para isso tenha de ser o meu corpo e a (minha alma) a lenha desse fogo. Só quero torná-la de toda a humanidade;ainda que para isso tenha de a perder como minha. Cada vez mais assim penso.Cada vez mais ponho da essência anímica do meu sangueo propósito impessoal de engrandecer a pátria e contribuirpara a evolução da humanidade.É a forma que em mim tomou o misticismo da nossa Raça.','(2)Voltei-me para ela; Capitu tinha os olhos no chão. Ergueu-os logo, devagar, e ficamos a olhar um para o outro... Confissão de crianças, tu valias bem duas ou três páginas, mas quero ser poupado. Em verdade, não falamos nada; o muro falou por nós. Não nos movemos, as mãos é que se estenderam pouco a pouco, todas quatro, pegando-se, apertando-se, fundindo-se. Não marquei a hora exata daquele gesto. Devia tê-la marcado; sinto a falta de uma nota escrita naquela mesma noite, e que eu poria aqui com os erros de ortografia que trouxesse, mas não traria nenhum, tal era a diferença entre o estudante e o adolescente. Conhecia as regras do escrever, sem suspeitar as do amar; tinha orgias de latim e era virgem de mulheres.','(3)NOSSA alegria diante dum sistema metafisico, nossa satisfação em presença duma construção do pensamento, em que a organização espiritual do mundo se mostra num conjunto lógico, coerente a harmônico, sempre dependem eminentemente da estética; têm a mesma origem que o prazer, que a alta satisfação, sempre serena afinal, que a atividade artística nos proporciona quando cria a ordem e a forma a nos permite abranger com a vista o caos da vida, dando-lhe transparência.']
+    #textos = ['a 1, oi teu bem. ola','b 2, teste .paranaue','c 3, teste1 .gingolo']
+    textos = ['a b c','a b c']
+    print('quantidade de textos ',len(textos))
+    print(textos)
+    sentencas = []
+    cont = 0
+    print('separando textos')
+    for texto in textos:
+        sentencas.append(separa_sentencas(texto))
+        cont += 1
+    print(sentencas)
+    print('quantidade de sentencas ',cont)
+    cont = 0
+    frases_fora = []
+    frases_dentro = []
+    frases = []
+    for frases_fora in sentencas:
+        for frases_dentro in frases_fora:
+            frases.append(separa_frases(frases_dentro))
+            cont += 1
+    print(frases)
+    print('quantidade de frases ', cont)
+    cont = 0
+    palavras = []
+    palavra_fora = []
+    palavra_dentro = []
+    
+    for palavra_fora in frases:
+        for palavra_dentro in palavra_fora:
+            palavras.append(separa_palavras(palavra_dentro))
+            cont += 1
+    print(palavras)
+    print('quantidade de palavras ', cont)
+    
+def main1():
+    # textos = le_textos()# faz a leitura dos textos e armazena em textos
+    # textos = ['(1)Navegadores antigos tinham uma frase gloriosa:"Navegar é preciso; viver não é preciso". Quero para mim o espírito [d]esta frase, transformada a forma para a casar como eu sou: Viver não é necessário; o que é necessário é criar. Não conto gozar a minha vida; nem em gozá-la penso. Só quero torná-la grande,ainda que para isso tenha de ser o meu corpo e a (minha alma) a lenha desse fogo. Só quero torná-la de toda a humanidade;ainda que para isso tenha de a perder como minha. Cada vez mais assim penso.Cada vez mais ponho da essência anímica do meu sangueo propósito impessoal de engrandecer a pátria e contribuirpara a evolução da humanidade.É a forma que em mim tomou o misticismo da nossa Raça.','(2)Voltei-me para ela; Capitu tinha os olhos no chão. Ergueu-os logo, devagar, e ficamos a olhar um para o outro... Confissão de crianças, tu valias bem duas ou três páginas, mas quero ser poupado. Em verdade, não falamos nada; o muro falou por nós. Não nos movemos, as mãos é que se estenderam pouco a pouco, todas quatro, pegando-se, apertando-se, fundindo-se. Não marquei a hora exata daquele gesto. Devia tê-la marcado; sinto a falta de uma nota escrita naquela mesma noite, e que eu poria aqui com os erros de ortografia que trouxesse, mas não traria nenhum, tal era a diferença entre o estudante e o adolescente. Conhecia as regras do escrever, sem suspeitar as do amar; tinha orgias de latim e era virgem de mulheres.','(3)NOSSA alegria diante dum sistema metafisico, nossa satisfação em presença duma construção do pensamento, em que a organização espiritual do mundo se mostra num conjunto lógico, coerente a harmônico, sempre dependem eminentemente da estética; têm a mesma origem que o prazer, que a alta satisfação, sempre serena afinal, que a atividade artística nos proporciona quando cria a ordem e a forma a nos permite abranger com a vista o caos da vida, dando-lhe transparência.']
+    textos = ['a 1, oi teu bem. ola','b 2, teste .paranaue','c 3, teste1 .gingolo']
+    #textos = ['a. b. c', 'a. b. c']
+    print('quantidade de textos ', len(textos))
+    print(textos)
+    
+    cont = 0
+
+    
+    for texto in textos:
+        sentencas = separa_sentencas(texto)
+        cont += 1
+        for i in sentencas:
+            print(i)
+    '''
+    print(sentencas)
+    print('quantidade de sentencas ', cont)
+    cont = 0
+    frases_fora = []
+    frases_dentro = []
+    frases = []
+    for frases_fora in sentencas:
+        for frases_dentro in frases_fora:
+            frases.append(separa_frases(frases_dentro))
+            cont += 1
+    print(frases)
+    print('quantidade de frases ', cont)
+    cont = 0
+    palavras = []
+    palavra_fora = []
+    palavra_dentro = []
+    
+    for palavra_fora in frases:
+        for palavra_dentro in palavra_fora:
+            palavras.append(separa_palavras(palavra_dentro))
+            cont += 1
+    print(palavras)
+    print('quantidade de palavras ', cont)
+    '''
+
+
+main1()
