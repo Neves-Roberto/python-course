21915e4b78e87221d18d660225343d69f5eb38bd
dkscript <dkscript@gmail.com>
2019-09-26 14:14:39 -0300

    atualizacao opec

--- download_api_globo.py
+++ download_api_globo.py
@@ -25,10 +25,11 @@ path_api = 'D:\\ftp\\SISCOM\\' # destino dos aquivos de .py fonte
 #_______________________________________________________________________
 
 
+
 class PythonCornerExample(SMWinservice):
-    _svc_name_ = "Download_api_globo"
-    _svc_display_name_ = "Servido de download da api da globo"
-    _svc_description_ = "Este servico baixa automaticamente os arquivos da lista de comerciais da API da GLOBO"
+    _svc_name_ = "PythonCornerExample"
+    _svc_display_name_ = "Python Corner's Winservice Example"
+    _svc_description_ = "That's a great winservice! :)"
 
     def start(self):
         self.isrunning = True
@@ -39,12 +40,6 @@ class PythonCornerExample(SMWinservice):
     def main(self):
         i = 0
         while self.isrunning:
-            # _______________________________________________________________________
-            #
-            #
-            #   BLOCO DE PROGRAMA EM LOOP
-            #
-            # _______________________________________________________________________
             contador_tentativas = 0
             while contador_tentativas <= 3:
 
@@ -55,34 +50,37 @@ class PythonCornerExample(SMWinservice):
                 lista_ok = []
                 # Atualizando a lista_ok baseado no arquivo listadownload_ok.txt
                 try:
-                    arq_lista_ok = open(path_api + 'listadownload_ok.txt', 'r')
+                    arq_lista_ok = open(path + 'listadownload_ok.txt', 'r')
                     for linha in arq_lista_ok:
                         lista_ok.append(int(linha.replace('\n', '')))
-                    # print("LISTA DE MATERIAL JA BAIXADO E OK!")
-                    # print(lista_ok)
+                    print("LISTA DE MATERIAL JA BAIXADO E OK!")
+                    print(lista_ok)
                     arq_lista_ok.close()
                 except:
-                    lista_nao_ok = []
-
+                    print('Problema ao ler o arquivo download ok!')
+                lista_nao_ok = []
                 # Atualizando a lista_nao_ok baseado no arquivo listadownload_bad.txt
                 try:
-                    arq_lista_nao_ok = open(path_api + 'listadownload_bad.txt', 'r')
+                    arq_lista_nao_ok = open(path + 'listadownload_bad.txt', 'r')
                     for linha in arq_lista_nao_ok:
                         lista_nao_ok.append(int(linha.replace('\n', '')))
-                    # print('LISTA DE MATERIAL BAD!')
-                    # print(lista_nao_ok)
+                    print('LISTA DE MATERIAL BAD!')
+                    print(lista_nao_ok)
                     arq_lista_nao_ok.close()
                     # limpando o arquivo listadownload_bad.txt para atualizacao dentro do bloco de checagem de download
-                    arq_lista_nao_ok = open(path_api + 'listadownload_bad.txt', 'w')
+                    arq_lista_nao_ok = open(path + 'listadownload_bad.txt', 'w')
                     arq_lista_nao_ok.close()
                 except:
-                    # print("Problema ao ler o arquivo de download bad!")
-                    # TODO: apagar o arquivo para nova lista ser gerada
-                    data_de = datetime.datetime.strptime(str(datetime.datetime.now().year) + '-' + str(datetime.datetime.now().month) + '-' + str(datetime.datetime.now().day), "%Y-%m-%d")
-                    novo_data_de = data_de - dateutil.relativedelta.relativedelta(days=60)
-                    data = str(novo_data_de.year) + "-" + str(novo_data_de.month) + "-" + str(novo_data_de.day)
+                    print("Problema ao ler o arquivo de download bad!")
+                # TODO: apagar o arquivo para nova lista ser gerada
 
-                    # TODO:VERIFICAR MATERIAL DA CASA VATICANO, O QUE FALTA PARA COMPLETAR O ENDERECO, POIS NÃO FAZ DOWNLOAD
+                data_de = datetime.datetime.strptime(
+                    str(datetime.datetime.now().year) + '-' + str(datetime.datetime.now().month) + '-' + str(
+                        datetime.datetime.now().day), "%Y-%m-%d")
+                novo_data_de = data_de - dateutil.relativedelta.relativedelta(days=0)
+                data = str(novo_data_de.year) + "-" + str(novo_data_de.month) + "-" + str(novo_data_de.day)
+
+                # TODO:VERIFICAR MATERIAL DA CASA VATICANO, O QUE FALTA PARA COMPLETAR O ENDERECO, POIS NÃO FAZ DOWNLOAD
 
                 # obtendo lista atualizada do siscom
                 lista_material_opec = opec.GetMateriais(dataDe=[data], cdMateriais=[])
@@ -90,16 +88,16 @@ class PythonCornerExample(SMWinservice):
                 # listando e comparando com lista existente
 
                 lista_nova_tentativa = []
-                # print('contudo da lista nova tentativa')
-                # print(lista_nova_tentativa)
+                print('contudo da lista nova tentativa')
+                print(lista_nova_tentativa)
                 for material in lista_material_opec:
 
                     if material['codMaterial'] in lista_ok:
-                        pass
+                        print(str(material['codMaterial']) + " CODIGO DE MATERIAL JA EXISTE ")
                     else:
                         # adicionar na lista de nova tentativa de download
                         lista_nova_tentativa.append(material['codMaterial'])
-                        # print(str(material['codMaterial']) + " CODIGO DE MATERIAL NAO EXISTE - NOVA TENTATIVA ")
+                        print(str(material['codMaterial']) + " CODIGO DE MATERIAL NAO EXISTE - NOVA TENTATIVA ")
 
                 lista_material_opec = opec.GetMateriais(dataDe=[data], cdMateriais=[lista_nova_tentativa])
 
@@ -107,54 +105,55 @@ class PythonCornerExample(SMWinservice):
                     lista_material_opec.clear()  # Limpando a lista de download quando nao nescessita de tentativas
 
                 for material in lista_material_opec:
-                    # print(material)
+                    print(material)
                     nomeMaterial = material['nomeArquivo'].replace(' ',
                                                                    '_')  # retira os espacos do nome do arquivo, substitui por underline
                     md5_original = material['md5']
 
                     try:
-                        # print('Iniciando o download do arquivo ' + nomeMaterial)
+                        print('Iniciando o download do arquivo ' + nomeMaterial)
                         url = str(opec.GetEnderecos('1', str(material['idMaterial']))['enderecos'][0])
                     except:
-                        pass
+                        print('Falha em obter o endereco do arquivo ' + nomeMaterial)
 
                     if material['player'] == 'VATI':  # Caso de servidor com requisicao de certificado
                         try:
 
                             # Iniciar download alternativo
-                            # print('Baixando ' + nomeMaterial)
-                            # print(url)
+                            print('Baixando ' + nomeMaterial)
+                            print(url)
                             baixar_arquivo_alternativo(url, path + nomeMaterial)
                             # Insere na lista de donwloads ok
-                            arquivo_donwload = open(path_api + 'listadownload_ok.txt', 'a+')
+                            arquivo_donwload = open(path + 'listadownload_ok.txt', 'a+')
                             arquivo_donwload.write(str(material['codMaterial']) + '\n')
                             arquivo_donwload.close()
 
 
                         except:
 
-                            # print('Falha ao fazer donwload do arquivo ' + nomeMaterial)
+                            print('Falha ao fazer donwload do arquivo ' + nomeMaterial)
                             # Insere na lista de arquivos com problema no download
-                            arquivo_donwload = open(path_api + 'listadownload_bad.txt', 'a+')
+                            arquivo_donwload = open(path + 'listadownload_bad.txt', 'a+')
                             arquivo_donwload.write(str(material['codMaterial']) + '\n')
                             arquivo_donwload.close()
 
                     else:
                         # Bloco de download padrao
                         try:
-                            # print('Baixando ' + nomeMaterial)
-                            # print(url)
+                            print('Baixando ' + nomeMaterial)
+                            print(url)
                             wget.download(url, path + nomeMaterial)
 
                             # Insere na lista de donwloads ok
-                            arquivo_donwload = open(path_api + 'listadownload_ok.txt', 'a+')
+                            arquivo_donwload = open(path + 'listadownload_ok.txt', 'a+')
                             arquivo_donwload.write(str(material['codMaterial']) + '\n')
                             arquivo_donwload.close()
 
                         except:
-                            # print('Falha ao fazer donwload do arquivo ' + nomeMaterial)
+                            print('Falha ao fazer donwload do arquivo ' + nomeMaterial)
+
                             # Insere na lista de arquivos com problema no download
-                            arquivo_donwload = open(path_api + 'listadownload_bad.txt', 'a+')
+                            arquivo_donwload = open(path + 'listadownload_bad.txt', 'a+')
                             arquivo_donwload.write(str(material['codMaterial']) + '\n')
                             arquivo_donwload.close()
 
@@ -163,20 +162,19 @@ class PythonCornerExample(SMWinservice):
                         md5_arquivo = hashlib.md5(arquivo)
                         md5_final = md5_arquivo.hexdigest()
                     except:
-                        # print('Não foi possivel chevar a intergridade do arquivo ' + nomeMaterial)
+                        print('Não foi possivel chevar a intergridade do arquivo ' + nomeMaterial)
                         md5_final = ''
 
                     if md5_original == md5_final:
-                        pass
+                        print('ARQUIVO INTEGRO')
+                        print(md5_final)
                     else:
-                        pass
+                        print('ARQUIVO COM PROBLEMA')
 
-                time.sleep(120)  # tempo de espera para a proxima consulta
-
-
+                time.sleep(120)
 #_______________________________________________________________________
 if __name__ == '__main__':
-    download_api_globo.parse_command_line()
+    PythonCornerExample.parse_command_line()
 
 # pip install pywin32
 
