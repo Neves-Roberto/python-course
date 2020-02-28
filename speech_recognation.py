71db7a5ce7d1b7032e35a29ba20822f403d143fc
dkscript <dkscript@gmail.com>
2020-02-28 13:53:14 -0300

    atualizacao opec

--- speech_recognation.py
+++ speech_recognation.py
@@ -1,8 +1,9 @@
+
 import speech_recognition as sr
 
 
 def main():
-    sound = "C:\\Users\\projetos\\recorder.wav"
+    sound = "C:\\Users\\projetos\\recorder8.wav"
 
     r = sr.Recognizer()
 
@@ -23,3 +24,29 @@ def main():
 
 if __name__ == "__main__":
     main()
+    
+"""
+    
+import speech_recognition as sr
+AUDIO_FILE = ("C:\\Users\\projetos\\recorder.wav")
+r = sr.Recognizer()
+with sr.AudioFile(AUDIO_FILE) as source:
+    audio = r.record(source)
+
+try:
+    print("The audio file contains: " + r.recognize_google(audio,language='pt-BR'))
+
+except sr.UnknownValueError:
+    print("Google Speech Recognition could not understand audio")
+
+except sr.RequestError as e:
+    print("Could not request results from Google Speech Recognition service; {0}".format(e))
+
+try:
+    print("The audio file contains: " + r.recognize_sphinx(audio))
+except sr.UnknownValueError:
+    print("Sphinx could not understand audio")
+except sr.RequestError as e:
+    print("Sphinx error; {0}".format(e))
+
+"""
